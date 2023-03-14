93b77487f8ffdcea90bba9c4942d4c5a0ddccf44
esterandreetto <esterandreetto@localhost>
2023-03-14 13:43:41 +0100

    inizio

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -7,8 +7,35 @@ package it.polito.tdp.alien;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    private Button btnTranslate;
+
+    @FXML
+    private TextField insertTxt;
+
+    @FXML
+    private TextArea txtComm;
+    
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTranslation(ActionEvent event) {
+
+    }
+	
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -18,6 +45,10 @@ public class FXMLController {
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert insertTxt != null : "fx:id=\"insertTxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtComm != null : "fx:id=\"txtComm\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
